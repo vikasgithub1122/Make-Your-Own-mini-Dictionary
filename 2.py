6ba4c5c29eb637ac826d9cf5c618a5ae395c0c62
vikasgithub1122 <109681282+vikasgithub1122@users.noreply.github.com>
2023-02-27 21:45:03 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -10,3 +10,14 @@ print("-----Or----")
 
 my_dict = {chr(i): i for i in range(97, 97 + 26)}
 print(my_dict)
+
+
+print("-------------------")
+
+
+a='a'
+my_dict= {}
+for i in range(ord(a), ord('z')+1):
+    #  ASCII values
+    my_dict[chr(i)]=i
+print(my_dict)
